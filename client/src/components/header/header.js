b0e7d13554bd7a3d2eca986cee67d3e4b2cb260a
MTLY-1610 <57067429+MTLY-1610@users.noreply.github.com>
2020-05-27 21:33:22 +0200

    header fix

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,26 +1,30 @@
 import React from "react";
 import "./header.css";
 import Link from "react-router-dom";
+import {ReactComponent as Icon} from './shopping-basket-light.svg';
 
 class Header extends React.Component {
   state = {};
   render() {
     return (
       <header>
-          <h1 id="header">
-            Jannes
-            <br /> Tapeter
-          </h1>
-          <div id="navbar">
-            <h5>
-              Registrera / Logga in <span id="shoppingbag-logo">X</span>
-            </h5>
-            <ul>
-              <li>Tapeter</li>
-              <li>Barnrum</li>
-              <li>Tillbehör</li>
-            </ul>
+        <div id="container-header">
+          <div id="logo"> 
+            Jannes <br /> Tapeter
           </div>
+          <div id="container-right">
+            <div id="loggin"> 
+              
+            <span >Registrera</span><span > Logga in </span><span id=""><img className="basket" src={Icon} alt="product" /></span>
+              
+            </div>
+            <div id="menu"> 
+              <span>Tapeter</span>
+              <span>Barnrum</span>
+              <span>Tillbehör</span>
+            </div>
+          </div>
+        </div>
       </header>
     );
   }
