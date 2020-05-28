c2599588d5c30e5d337478cc42fcfdf2560a4685
Embla <emblajonsson@gmail.com>
2020-05-28 14:39:03 +0200

    admin stuff

--- client/src/components/admindashboard/adminDashboard.js
+++ client/src/components/admindashboard/adminDashboard.js
@@ -10,6 +10,27 @@ class AdminDashboard extends React.Component {
             <React.Fragment>
                 <Header />
                 <h2>Admin sida</h2>
+                <div>
+                    <div>
+                        <h2>Handle Users</h2>
+                        <p>Accept as admin user</p>
+                        <p>Denie admin access</p>
+                        <p></p>
+                    </div>
+                    <div>
+                        <h2>Handle Product quantity</h2>
+                        <p>update qunatity</p>
+                        <p>Delete product</p>
+                        <p>Add product</p>
+                        <p>Change category for product</p>
+                    </div>
+                    <div>
+                        <h2>All orders</h2>
+                        <p>mark order as sent</p>
+                        <p>All orders that have been made</p>
+
+                    </div>
+                </div>
                 <Footer />
             </React.Fragment>
         );
