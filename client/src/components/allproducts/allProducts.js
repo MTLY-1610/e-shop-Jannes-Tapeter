d8258ecbe9e12fab04bdb05beaa8ba55f5e776e3
MTLY-1610 <57067429+MTLY-1610@users.noreply.github.com>
2020-05-29 13:01:51 +0200

    basics productCategory

--- client/src/components/allproducts/allProducts.js
+++ client/src/components/allproducts/allProducts.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Header from "../header/header";
+import Footer from "../footer/footer";
+import Hero from "../homepage/hero.jpeg";
+import "./allProducts.css";
 
 class regularProducts extends React.Component {
   state = {};
   render() {
-    return <h3>ALL PRODUCTS</h3>;
+    return  (
+    <React.Fragment> 
+    <Header />
+    <img className="hero" src={Hero} alt="hero" />
+    
+    <Footer />
+  </React.Fragment>
+    )
   }
 }
+      
 
 export default regularProducts;
