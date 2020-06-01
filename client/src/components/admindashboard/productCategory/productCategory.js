bfc17dcf45552d7fe16cebfd8f01832dc59d3069
embla <emblajonsson@gmail.com>
2020-06-01 21:31:01 +0200

    state product

--- client/src/components/admindashboard/productCategory/productCategory.js
+++ client/src/components/admindashboard/productCategory/productCategory.js
@@ -1,16 +1,34 @@
 import React from "react";
-import "./productCategory.css" 
+import "./productCategory.css"
 
 //Administratörer ska kunna redigera vilka kategorier en produkt tillhör (VG)
 
 class ProductCategory extends React.Component {
-    render() {
-      return (
-        <div id="content-container">
-            <h3>Här ska man kunna redigera vilka kategorier en produkt tillhör</h3>
-        </div>
-      );
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      product: [{
+        category: [],
+        quantity: 0,
+        url: "",
+        price: 0,
+        description: "",
+        brand: "",
+        designer: "",
+        ref: "",
+        dimensions: ""
+      }]
     }
+  }
+
+  render() {
+    return (
+      <div id="content-container">
+        <h3>Här ska man kunna redigera vilka kategorier en produkt tillhör</h3>
+      </div>
+    );
+  }
 }
-  
+
 export default ProductCategory;
