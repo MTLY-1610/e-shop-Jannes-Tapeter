5735ad52557f1702bf8fb874ea0d621382f869ba
FThelin <fredrikthelin82@gmail.com>
2020-06-02 17:12:26 +0200

    added kr

--- client/src/components/product card/productCard.js
+++ client/src/components/product card/productCard.js
@@ -26,7 +26,7 @@ class ProductCard extends React.Component {
           </div>
           <div id="description-col2">
             <div id="left">{this.props.designer}</div>
-            <div id="left">{this.props.price}</div>
+            <div id="left">{`${this.props.price} kr`}</div>
           </div>
         </div>
       </div>
