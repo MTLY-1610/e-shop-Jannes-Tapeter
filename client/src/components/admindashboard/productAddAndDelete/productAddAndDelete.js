286f411bca6bebff4c231b8c90e2565728b3bc28
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-06-03 16:01:32 +0200

    start on productAddAndDelete component

--- client/src/components/admindashboard/productAddAndDelete/productAddAndDelete.js
+++ client/src/components/admindashboard/productAddAndDelete/productAddAndDelete.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./productAddAndDelete.css";
+import { TextField, Button } from "@material-ui/core";
 import Header from "../../header/header";
 import Footer from "../../footer/footer";
 import Sidebar from "../sidebar/sidebar";
 
 //Administratörer ska kunna lägga till och ta bort produkter (VG)
+/* category: [String],
+quantity: Number,
+url: String,
+price: Number,
+description: String,
+brand: String,
+designer: String,
+ref: String,
+dimensions: String,
+date: Number, */
 
 class ProductAddAndDelete extends React.Component {
   render() {
@@ -14,6 +25,19 @@ class ProductAddAndDelete extends React.Component {
         <Sidebar />
         <div id="content-container">
           <h3>Här ska man kunna lägga till och ta bort produkter</h3>
+          <TextField
+                size="small"
+                id="outlined-basic"
+                variant="outlined"
+                margin="dense"
+              />
+              <Button
+                        id="formularButton"
+                        size="small"
+                        variant="contained"
+                      >
+                        Lägg till produkt
+                      </Button>
         </div>
         <Footer />
       </React.Fragment>
