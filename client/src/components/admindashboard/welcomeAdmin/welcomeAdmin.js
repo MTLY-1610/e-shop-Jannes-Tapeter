ee3c6d506e5fea81b895d07e9ace96ac27c7524d
embla <emblajonsson@gmail.com>
2020-06-03 12:46:20 +0200

    welcome admin

--- client/src/components/admindashboard/welcomeAdmin/welcomeAdmin.js
+++ client/src/components/admindashboard/welcomeAdmin/welcomeAdmin.js
@@ -6,24 +6,74 @@ import Sidebar from "../sidebar/sidebar";
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { flexbox } from '@material-ui/system';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import SendIcon from '@material-ui/icons/Send';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import MoveToInboxIcon from '@material-ui/icons/MoveToInbox';
+import PlusOneIcon from '@material-ui/icons/PlusOne';
 
 const WelcomeAdmin = () => {
   return (
     <React.Fragment>
       <Header />
       <Sidebar />
-      <div style={{ width: '60%', border: '1px solid black' }}>
+      <div className="content-container">
         <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
           <Typography variant="h3" component="h3" gutterBottom>
             Välkommen Admin!
           </Typography>
           <Typography component="div">
-            <Box textAlign="center" m={1}>
-              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-              The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-      </Box>
-          </Typography>
+            Du som inloggad administratör har speciella rättigheter som en vanlig användare inte har.
+            Du kan se i menyn till vänster vad du har möjlighet att göra.
+              </Typography>
+          <List>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <SendIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Du kan markera en order som skickad" />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <PlusOneIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Ändra lagersaldot på en produkt" />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <MoveToInboxIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Flytta eller lägga till en produkt i en specifik kategori" />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <ShoppingCartIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Lägga till en produkt i shoppen eller ta bort en produkt" />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <PersonAddIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Acceptera användare som ansöker om att bli administratörer
+"/>
+            </ListItem>
+          </List>
         </Box>
       </div>
       <Footer />
