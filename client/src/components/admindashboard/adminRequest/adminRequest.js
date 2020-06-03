7fb6e721a8a2390a6f4c5b7415f54ed6fd2caafb
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-06-03 13:00:11 +0200

    working on adminRequest

--- client/src/components/admindashboard/adminRequest/adminRequest.js
+++ client/src/components/admindashboard/adminRequest/adminRequest.js
@@ -1,8 +1,15 @@
 import React from "react";
+import {
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  Button
+} from "@material-ui/core";
 import "./adminRequest.css";
 import Header from "../../header/header";
 import Footer from "../../footer/footer";
 import Sidebar from "../sidebar/sidebar";
+
 
 //En administratör behöver godkännas av en tidigare administratör innan man kan logga in fösta gången (VG)
 
@@ -13,10 +20,24 @@ class AdminRequest extends React.Component {
         <Header />
         <Sidebar />
         <div id="content-container">
-          <h3>
-            Här ska man kunna godkänna användare som har sänt in en förfrågan om
-            att bli administratör
-          </h3>
+          <h3>Förfrågningar om att bli administratör:</h3>
+          <div id="row-container">
+            <h4>Ny admin</h4>
+            <h4>epost</h4>
+            <RadioGroup row id="radioGroup">
+              <FormControlLabel
+                control={<Radio color="default" size="small" />}
+                value="Godkänn" label="Godkänd" labelPlacement="start"
+              />
+              <FormControlLabel
+                control={<Radio color="default" size="small" />}
+                value="Denied"  label="Nekad" labelPlacement="start"
+              />
+            </RadioGroup>
+            <Button id="formularButton" size="small" variant="contained">
+              Sänd
+            </Button>
+          </div>
         </div>
         <Footer />
       </React.Fragment>
