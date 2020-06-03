0fcd6cc805c210c7e1a3ca2cbbbec10d0780589c
embla <emblajonsson@gmail.com>
2020-06-03 16:02:49 +0200

    stock work in progress

--- client/src/components/admindashboard/productStockBalance/productStockBalance.js
+++ client/src/components/admindashboard/productStockBalance/productStockBalance.js
@@ -3,20 +3,35 @@ import "./productStockBalance.css";
 import Header from "../../header/header";
 import Footer from "../../footer/footer";
 import Sidebar from "../sidebar/sidebar";
+import { ProductConsumer } from "../../../context/productContext";
 
 //Administratörer ska kunna uppdatera antalet produkter i lager från admin delen av sidan (G)
 
 class ProductStockBalance extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <Header />
-        <Sidebar />
-        <div id="content-container">
-          <h3>Här ska man kunna uppdatera antalet produkter i lager</h3>
-        </div>
-        <Footer />
-      </React.Fragment>
+      <ProductConsumer>
+        {(product) => (
+          <React.Fragment>
+            <Header />
+            <Sidebar />
+            <div id="content-container">
+              <h3>Lager saldo:</h3>
+              {product.state.allProducts.map(
+                (product) =>
+                  (
+                    <div key={product._id}>
+                      <img></img>
+                      <h4>Namn: {product.ref}</h4>
+                      <h5>Antal: {product.quantity}</h5>
+                    </div>
+                  )
+              )}
+            </div>
+            <Footer />
+          </React.Fragment>
+        )}
+      </ProductConsumer>
     );
   }
 }
