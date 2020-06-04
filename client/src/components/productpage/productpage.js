33c2f4e26d884d9d5a75224328af5be1e5eb556e
MTLY-1610 <57067429+MTLY-1610@users.noreply.github.com>
2020-06-04 17:53:36 +0200

    basic layout

--- client/src/components/productpage/productpage.js
+++ client/src/components/productpage/productpage.js
@@ -1,8 +1,41 @@
 import React from "react";
+import "./productpage.css"
+import Link from "react-router-dom";
+import Header from "../header/header";
+import Footer from "../footer/footer";
+import sample from "./product-sample.png";
+
+
 
 class ProductPage extends React.Component {
+  serverUrl = "http://localhost:5000/";
+
   render() {
-    return <h3>PRODUKT</h3>;
+    return (
+      <React.Fragment> 
+      <Header />
+      <div id="pp-container">
+          <div id="img-container">
+            <img
+                  className="sample"
+                  src={sample}
+                  alt="product"
+                />
+            
+          </div>
+          <div id="desc-container">
+            <div id="brand-row"></div>
+            <div id="basics-row"></div>
+            <div id="specs-row"></div>
+            <div id="descr-row"></div>
+            <div id="action-row"></div>
+          </div>
+      </div>
+     
+
+      <Footer />
+    </React.Fragment>
+      );
   }
 }
 
