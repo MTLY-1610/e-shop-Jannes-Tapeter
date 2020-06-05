af5690ffb46eb93538b3b4bfedb028a2107caa2f
MTLY-1610 <57067429+MTLY-1610@users.noreply.github.com>
2020-06-05 09:48:49 +0200

    descr finished

--- client/src/components/productpage/productpage.js
+++ client/src/components/productpage/productpage.js
@@ -3,13 +3,24 @@ import "./productpage.css"
 import Link from "react-router-dom";
 import Header from "../header/header";
 import Footer from "../footer/footer";
-import sample from "./product-sample.png";
+import sample from "./sample-product.png";
 import logo from "./brand-sample.png";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 
 
 
 class ProductPage extends React.Component {
   serverUrl = "http://localhost:5000/";
+  
+
+  
 
   render() {
     return (
@@ -33,20 +44,55 @@ class ProductPage extends React.Component {
                   />
                 </div>
             </div>
-            <div id="specs-row">
-            
-            <div id="specs-col1">
-              <div id="model">B. D. 7</div>
-              <div id="ref">réf: 88666</div>
+            <div id="descr-row">
+              <div id="specs-col1">
+                <div id="model">B. D. 7</div>
+                <div id="ref">réf: 88666</div>
+              </div>
+              <div id="specs-col2">
+                <div id="price"> 8000 kr</div>
+              </div>
             </div>
-            <div id="specs-col2">
-              <div id="price"> 8000 kr</div>
+            <div id="origin-row">
+            <div id="origin-col1">
+              <div id="dim">260 cm x 300cm</div>
+              </div>
+              <div id="origin-col2">
+                <div id="designer"> Designer:</div>
+                <div id="designer-name"> Bruno Defontaine</div>
+              </div>
             </div>
-            
+            <div id="text-container">Labo Leonard,designers Nathalie Guillot and Bruno Defontaine volvo abba burzum saab kaleskaviar spendrups  volvo abba burzum saab kaleskaviar spendrups  volvo abba burzum saab kaleskaviar spendrups  volvo abba burzum saab kaleskaviar spendrups  </div>
+            <div id="action-row">
+              <div id="action-col1">
+                <FormControl >
+                  <Select
+                    
+                  >
+                    <MenuItem value="quantity" disabled>
+                    </MenuItem>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={6}>6</MenuItem>
+                    <MenuItem value={7}>7</MenuItem>
+                    <MenuItem value={8}>8</MenuItem>
+                    <MenuItem value={9}>9</MenuItem>
+                    <MenuItem value={10}>10</MenuItem>
+                    <MenuItem value={11}>11</MenuItem>
+                    <MenuItem value={12}>12</MenuItem>
+                  </Select>
+                  <FormHelperText>Quantity</FormHelperText>
+                </FormControl>
+              </div>
+              <div id="action-col2">
+                <Button variant="contained" size="small" color="#F5F5F5" >
+                    add to cart
+                </Button>
+              </div>  
             </div>
-            <div id="specs-row"></div>
-            <div id="descr-row"></div>
-            <div id="action-row"></div>
           </div>
       </div>
      
