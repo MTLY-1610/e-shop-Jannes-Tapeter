3439da99ba891bcee8932db08c9300b03a443b67
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-06-10 12:35:35 +0200

    small changes

--- client/src/components/admindashboard/productCategory/productCategory.js
+++ client/src/components/admindashboard/productCategory/productCategory.js
@@ -6,8 +6,6 @@ import Sidebar from "../sidebar/sidebar";
 import { Button, Modal, FormHelperText } from "@material-ui/core";
 import { ProductConsumer } from "../../../context/productContext";
 
-
-
 /* category: [String],
 quantity: Number,
 url: String,
@@ -30,115 +28,113 @@ class ProductCategory extends React.Component {
   }
   serverUrl = "http://localhost:5000/";
 
-  changeCategory(id, brand) {
+  removeProduct(id, brand) {
     this.setState({ productBrand: brand, openConfirmationWindow: true });
   }
 
-
   render() {
-    return ( <ProductConsumer>
-      {(products, category) => (
-        <React.Fragment>
-          <Header />
-          <Sidebar />
-          <div id="content-container">
-            
-            <h2>Alla produkter</h2>
-            {products.state.allProducts.map((product) => (
-              <div id="delete-product-card" key={product._id}>
-                <img
-                  className="sample"
-                  src={`${this.serverUrl}${product.url}`}
-                  alt="product"
-                />
-                <div>
-                  <span>Märke</span> {product.brand}
+    return (
+      <ProductConsumer>
+        {(products, category) => (
+          <React.Fragment>
+            <Header />
+            <Sidebar />
+            <div id="content-container">
+              <h2>Alla produkter</h2>
+              {products.state.allProducts.map((product) => (
+                <div id="product-card" key={product._id}>
+                  <img
+                    className="sample"
+                    src={`${this.serverUrl}${product.url}`}
+                    alt="product"
+                  />
+                  <div>
+                    <span>Märke</span> {product.brand}
+                  </div>
+                  <div>
+                    <span>Designer</span> {product.designer}
+                  </div>
+                  <div>
+                    <span>Ref</span> {product.ref}
+                  </div>
+                  <div>
+                    <span>Mått</span> {product.dimensions}
+                  </div>
+                  <div>
+                    <span>Antal</span> {product.quantity} st
+                  </div>
+                  <div>
+                    <span>Pris</span> {product.price}kr/st
+                  </div>
+                  <div class="category-div">
+                    <span>Kategori</span>
+                    {product.category.map((category, index) => (
+                      <div key={index}>
+                        {category}
+                        <Modal
+                          open={this.state.openConfirmationWindow}
+                          onClose={() =>
+                            this.setState({ openConfirmationWindow: false })
+                          }
+                          aria-labelledby="simple-modal-title"
+                          aria-describedby="simple-modal-description"
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                        >
+                          {
+                            <div
+                              style={{
+                                background: "white",
+                                alignItems: "center",
+                                flexDirection: "column",
+                                display: "flex",
+                                padding: "2rem",
+                              }}
+                            >
+                              <h1>
+                                {`Är du säker på att du vill redigera "${this.state.productBrand}" kategori?
+              `}
+                              </h1>
+                              <Button
+                                id="confirmationButton"
+                                size="small"
+                                variant="contained"
+                                onClick={() =>
+                                  products.deleteProduct(product._id)
+                                }
+                              >
+                                Ja, ta bort
+                              </Button>
+                            </div>
+                          }
+                        </Modal>
+                        <Button
+                          id="adminButton"
+                          size="small"
+                          variant="contained"
+                          onClick={() =>
+                            this.removeProduct(product._id, product.brand)
+                          }
+                        >
+                          Redigera kategori
+                        </Button>
+                      </div>
+                    ))}
+                  </div>
+                  <div>
+                    <span>Beskrivning</span> {product.description}
+                  </div>
                 </div>
-                <div>
-                  <span>Designer</span> {product.designer}
-                </div>
-                <div>
-                  <span>Ref</span> {product.ref}
-                </div>
-                <div>
-                  <span>Mått</span> {product.dimensions}
-                </div>
-                <div>
-                  <span>Antal</span> {product.quantity} st
-                </div>
-                <div>
-                  <span>Pris</span> {product.price}kr/st
-                </div>
-                <div>
-                  <span>Kategori</span>
-                  {product.category.map((category, index) => (
-                    <div key={index}>{category}
-                    
-                    </div>
-                  ))}
-                </div>
-                <div>
-                  <span>Beskrivning</span> {product.description}
-                </div>
-                <Modal
-                  open={this.state.openConfirmationWindow}
-                  onClose={() =>
-                    this.setState({ openConfirmationWindow: false })
-                  }
-                  aria-labelledby="simple-modal-title"
-                  aria-describedby="simple-modal-description"
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  {
-                    <div
-                      style={{
-                        background: "white",
-                        alignItems: "center",
-                        flexDirection: "column",
-                        display: "flex",
-                        padding: "2rem",
-                      }}
-                    >
-                      <h1>
-                        {`Är du säker på att du vill ta bort "${this.state.productBrand}" från
-                sortimentet`}
-                      </h1>
-                      <Button
-                        id="confirmationButton"
-                        size="small"
-                        variant="contained"
-                        onClick={() => products.changeCategory(product._id)}
-                      >
-                        Ja, ta bort
-                      </Button>
-                    </div>
-                  }
-                </Modal>
-                <Button
-                  id="adminButton"
-                  size="small"
-                  variant="contained"
-                  onClick={() =>
-                    this.removeProduct(product._id, product.brand)
-                  }
-                >
-                  Ta bort produkt
-                </Button>
-              </div>
-            ))}
-          </div>
-          <Footer />
-        </React.Fragment>
-      )}
-    </ProductConsumer>
-  );
-}
+              ))}
+            </div>
+            <Footer />
+          </React.Fragment>
+        )}
+      </ProductConsumer>
+    );
+  }
 }
 export default ProductCategory;
-
-
-
