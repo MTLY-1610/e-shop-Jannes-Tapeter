80d4044294fc19a54c7de503533cc7fb488ab94a
MTLY-1610 <57067429+MTLY-1610@users.noreply.github.com>
2020-06-10 12:15:34 +0200

    footer

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -9,37 +9,38 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <div id="logo">
-          <h1>
-          Jannes
-          <br />
-          Tapeter
-          </h1>
-        </div>
-        <div id="text-box">
-          <h6>Hos oss på Jannes Tapeter hittar ni kollektioner från världens mest exklusiva tapetdesigners. Tveka inte att höra av er till oss för rådgivning och hjälp. </h6>
-        </div>
-        <div id="social">
-            <Link href="mailto: info@jannestapeter.se">
-              <MailOutlineIcon />
-            </Link>
-            <Link target="_blank"  href="https://www.facebook.com/">
-              <FacebookIcon />
-            </Link>
-            <Link target="_blank"  href="https://www.instagram.com/">
-              <InstagramIcon />
-            </Link>
-            <Link  target="_blank" href="https://www.google.com/maps/place/Bygdeg%C3%A5rden/@57.4745103,13.2529158,15z/data=!4m13!1m7!3m6!1s0x465065439ad193fd:0xa01907aa3405a90!2s514+92+Uddebo!3b1!8m2!3d57.474512!4d13.2616706!3m4!1s0x465065f45f8ba5a5:0x9e8e3bcf05f13b86!8m2!3d57.4751313!4d13.2649371" >
-              <RoomIcon />
-            </Link>
-        </div>
-        <div id="contact">
+        <div id="footer-container">
+          <div id="logo">
+           
+            Jannes
+            <br />
+            Tapeter
+            
+          </div>
+          {/* <div id="text-box">
+            <h6>Hos oss på Jannes Tapeter hittar ni kollektioner från världens mest exklusiva tapetdesigners. Tveka inte att höra av er till oss för rådgivning och hjälp. </h6>
+          </div> */}
+          <div id="social">
+              <Link href="mailto: info@jannestapeter.se">
+                <MailOutlineIcon />
+              </Link>
+              <Link target="_blank"  href="https://www.facebook.com/">
+                <FacebookIcon />
+              </Link>
+              <Link target="_blank"  href="https://www.instagram.com/">
+                <InstagramIcon />
+              </Link>
+              <Link  target="_blank" href="https://www.google.com/maps/place/Bygdeg%C3%A5rden/@57.4745103,13.2529158,15z/data=!4m13!1m7!3m6!1s0x465065439ad193fd:0xa01907aa3405a90!2s514+92+Uddebo!3b1!8m2!3d57.474512!4d13.2616706!3m4!1s0x465065f45f8ba5a5:0x9e8e3bcf05f13b86!8m2!3d57.4751313!4d13.2649371" >
+                <RoomIcon />
+              </Link>
+          </div>
+          <div id="contact">
 
-            <h5>Jannes Tapeter</h5> 
-            <h5>Uddebogatan 9, 514 92 Uddebo</h5> 
-            <h5>+46 (0)70 123 45 67</h5>
+              <h5>Jannes Tapeter</h5> 
+              <h5>Uddebogatan 9, 514 92 - Uddebo</h5> 
+              <h5>+46 (0)70 123 45 67</h5>
+          </div>
         </div>
-
       </footer>
     );
   }
