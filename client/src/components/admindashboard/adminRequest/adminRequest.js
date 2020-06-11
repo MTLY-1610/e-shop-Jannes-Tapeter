81376a6d34b5ace1bdbe91c44d6c67f9714bbc4a
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-06-11 16:45:49 +0200

    adminrequest ok

--- client/src/components/admindashboard/adminRequest/adminRequest.js
+++ client/src/components/admindashboard/adminRequest/adminRequest.js
@@ -30,49 +30,61 @@ class AdminRequest extends React.Component {
           <React.Fragment>
             <Header />
             <div className="a-content-container">
-        <Sidebar />
-        <div className="a-child-content-container">
-              <h3>Förfrågningar om att bli administratör:</h3>
-              {customer.state.allCustomers.map(
-                (user) =>
-                  user.role === "wantsToBeAdmin" && (
-                    <div id="a-row-container" key={user._id}>
-                      <h4>{user.firstName}</h4>
-                      <h4>{user.lastName}</h4>
-                      <h4>{user.email}</h4>
-                      <RadioGroup row id="a-adminRadioGroup">
-                        <FormControlLabel
-                          control={<Radio color="default" size="small" />}
-                          value="approved"
-                          label="Godkänd"
-                          labelPlacement="start"
-                          onChange={() => this.setState({ approved: true })}
-                        />
-                        <FormControlLabel
-                          control={<Radio color="default" size="small" />}
-                          value="denied"
-                          label="Nekad"
-                          labelPlacement="start"
-                          onChange={() => this.setState({ approved: false })}
-                        />
-                      </RadioGroup>
-                      <Button
-                        id="a-adminButton"
-                        size="small"
-                        variant="contained"
-                        onClick={() =>
-                          customer.editCustomer({
-                            id: user._id,
-                            role: this.getRole(),
-                          })
-                        }
-                      >
-                        Sänd
-                      </Button>
-                    </div>
-                  )
-              )}
-            </div>
+              <Sidebar />
+              <div className="a-child-content-container">
+                <h3>Förfrågningar om att bli administratör:</h3>
+                {customer.state.allCustomers.map(
+                  (user) =>
+                    user.role === "wantsToBeAdmin" && (
+                      <div className="a-admin-request-card" key={user._id}>
+                        <div className="a-admin-request-card-name">
+                          <h4>
+                            <span>Namn: </span>
+                            {user.firstName} {user.lastName}
+                          </h4>
+                          <h4>
+                            <span>Melj: </span>
+                            {user.email}
+                          </h4>
+                        </div>
+                        <div className="a-admin-request-card-form">
+                          <RadioGroup row>
+                            <FormControlLabel
+                              control={<Radio color="default" size="small" />}
+                              value="approved"
+                              label="Godkänd"
+                              labelPlacement="start"
+                              onChange={() => this.setState({ approved: true })}
+                            />
+                            <FormControlLabel
+                              control={<Radio color="default" size="small" />}
+                              value="denied"
+                              label="Nekad"
+                              labelPlacement="start"
+                              onChange={() =>
+                                this.setState({ approved: false })
+                              }
+                            />
+                          </RadioGroup>
+                          <Button
+                            id="a-adminButton"
+                            size="small"
+                            variant="contained"
+                            onClick={() =>
+                              customer.editCustomer({
+                                id: user._id,
+                                role: this.getRole(),
+                              })
+                            }
+                          >
+                            Sänd
+                          </Button>
+                        </div>
+                       
+                      </div>
+                    )
+                )}
+              </div>
             </div>
             <Footer />
           </React.Fragment>
